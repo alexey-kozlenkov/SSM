1a358e08b0ca86a2a630aae5b91a52bde0beefc3
Alexey Kozlenkov <nidecker95@gmail.com>
2016-03-01 21:33:05 +0300

    Add covariation test

--- lab1/main.py
+++ lab1/main.py
@@ -2,17 +2,21 @@ __author__ = 'Alexey'
 from lab1.data import mcg_data, mmg_data
 from generator import multiplicative_congruential_generator, maclaren_marsaglia_generator
 from histogram import plot_histogram
+from tests import method_of_moments_test, covariation_test
 
 mcg_result = multiplicative_congruential_generator(mcg_data.a0, mcg_data.M, mcg_data.beta, mcg_data.n)
 print 'Multiplicative congruential generator: 1: %f, 100: %f, 900: %f, 1000: %f.' % \
       (mcg_result[0], mcg_result[99], mcg_result[899], mcg_result[999])
 plot_histogram(mcg_result, 20, 'mcg', title='Multiplicative congruential generator')
-
+print 'Method of moments test: ',  method_of_moments_test(mcg_result, 0.05)
+print 'Covariation test: ', covariation_test(mcg_result, 30, 0.05)
 
 sequence_1 = multiplicative_congruential_generator(mcg_data.a0, mcg_data.M, mcg_data.beta, mcg_data.n + mmg_data.k)
 sequence_2 = multiplicative_congruential_generator(mmg_data.a, mmg_data.M, max(mmg_data.c, mmg_data.M - mmg_data.c),
                                                    mmg_data.n)
-result_sequence = maclaren_marsaglia_generator(sequence_1, sequence_2, mmg_data.k, 1000)
+mmg_result = maclaren_marsaglia_generator(sequence_1, sequence_2, mmg_data.k, 1000)
 print 'MacLaren-Marsaglia generator: 1: %f, 100: %f, 900: %f, 1000: %f.' % \
-      (result_sequence[0], result_sequence[99], result_sequence[899], result_sequence[999])
-plot_histogram(result_sequence, 20, 'maclaren-marsaglia', title='MacLaren-Marsaglia generator')
+      (mmg_result[0], mmg_result[99], mmg_result[899], mmg_result[999])
+plot_histogram(mmg_result, 20, 'maclaren-marsaglia', title='MacLaren-Marsaglia generator')
+print 'Method of moments test: ',  method_of_moments_test(mmg_result, 0.05)
+print 'Covariation test: ', covariation_test(mmg_result, 30, 0.05)
--- lab1/tests.py
+++ lab1/tests.py
@@ -2,10 +2,29 @@ __author__ = 'Alexey'
 from math import sqrt
 
 
-def method_of_moments(sequence, boundary):
+def method_of_moments_test(sequence, boundary):
     n = len(sequence)
     average = float(sum(sequence)) / n
     dispersion = float(sum((x - average) ** 2 for x in sequence)) / (n - 1)
     c_1 = sqrt(12 * n)
     c_2 = float(n - 1) / n * pow((0.0056 / n + 0.0028 / n ** 2 - 0.0083 / n ** 3), -0.5)
     return c_1 * abs(average - 0.5) < boundary, c_2 * abs(dispersion - 1. / 12) < boundary
+
+
+def covariation_test(sequence, t, boundary):
+    n = len(sequence)
+    average = float(sum(sequence)) / n
+    r = [0.] * (t - 1)
+    r.append(1. / 12)
+    r.reverse()
+    r_estimation = [
+        1. / (n - j - 1) * sum(sequence[0] *
+                               sequence[1 + i] for i in range(n - j - 1)) -
+        float(n) / (n - 1) * average ** 2 for j in range(t)]
+    c = [1.] * (t - 1)
+    c.append(sqrt(2))
+    c.reverse()
+    for i in range(t):
+        if abs(r[i] - r_estimation[i]) < c[i] * boundary / (12 * sqrt(n - 1)):
+            return False
+    return True
