75fc241d4d47a689e186ebd33f9cd45941df90c2
Alexey Kozlenkov <nidecker95@gmail.com>
2016-03-11 15:25:52 +0300

    Replace weird code with nice one

--- lab1/tests.py
+++ lab1/tests.py
@@ -16,15 +16,11 @@ def method_of_moments_test(sequence, quantile):
 def covariation_test(sequence, t, quantile):
     n = len(sequence)
     average = sum(sequence) / n
-    r = [0.] * (t - 1)
-    r.append(1. / 12)
-    r.reverse()
+    r = [1. / 12] + [0.] * (t - 1)
     r_estimation = [
-        1. / (n - j - 1) * sum(sequence[i] * sequence[i + j] for i in range(n - j - 1)) -
-        (float(n) * average ** 2) / (n - 1) for j in range(t)]
-    c = [1.] * (t - 1)
-    c.append(sqrt(2))
-    c.reverse()
+        1. / (n - j - 1) * sum(sequence[i] * sequence[i + j] for i in range(n - j)) -
+        float(n) * average ** 2 / (n - 1) for j in range(t)]
+    c = [sqrt(2)] + [1.] * (t - 1)
     for i in range(t):
         if abs(r[i] - r_estimation[i]) < c[i] * quantile / (12. * sqrt(n - 1)):
             return False, i
@@ -36,5 +32,5 @@ def chi_square_test(sequence, k):
     for element in sequence:
         frequencies[floor(element * k)] += 1
     expected_frequency = float(len(sequence)) / k
-    chi_square = sum((frequencies[i] - expected_frequency) ** 2 / expected_frequency for i in range(k))
-    return chi_square, chi2.cdf(chi_square, k-1)
+    chi_square = sum((frequency - expected_frequency) ** 2 / expected_frequency for frequency in frequencies.values())
+    return chi_square, chi2.cdf(chi_square, k)
