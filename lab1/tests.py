076265859e0e3722301d0aae68b11b88ca12087d
Alexey Kozlenkov <nidecker95@gmail.com>
2016-03-13 14:47:21 +0300

    Replace weird code with nice one vol.2

--- lab1/tests.py
+++ lab1/tests.py
@@ -9,7 +9,7 @@ def method_of_moments_test(sequence, quantile):
     average = float(sum(sequence)) / n
     dispersion = float(sum((x - average) ** 2 for x in sequence)) / (n - 1)
     c_1 = sqrt(12 * n)
-    c_2 = float(n - 1) / n * pow((0.0056 / n + 0.0028 / n ** 2 - 0.0083 / n ** 3), -0.5)
+    c_2 = float(n - 1) / n * sqrt(0.0056 / n + 0.0028 / n ** 2 - 0.0083 / n ** 3)
     return c_1 * abs(average - 0.5) < quantile, c_2 * abs(dispersion - 1. / 12) < quantile
 
 
