19098502c1e44aaf23805714c2d56269e3b5b1ae
Alexey Kozlenkov <nidecker95@gmail.com>
2016-03-16 21:13:03 +0300

    Implement function to plot histogram with exact distribution graphic

--- util/plotter.py
+++ util/plotter.py
@@ -11,3 +11,19 @@ def plot_histogram(sequence, k, filename, title=None):
     plot.title(title if title else '')
     plot.savefig('../graphics/' + filename + '.png')
     plot.clf()
+
+
+def plot_histogram_with_exact_function(sequence, k, exact_y, exact_title, filename, title=None):
+    plot.xkcd()
+    plot.grid(True, linewidth=0.5, zorder=0)
+    n, bins, patches = plot.hist(sequence, k, zorder=3, label='Histogram')
+    plot.ylim(0, max(n) * 1.25)
+
+    bin_centers = [0.5 * (bins[i] + bins[i + 1]) for i in range(k)]
+    plot.plot(bin_centers, map(lambda y: y * len(sequence), exact_y[::len(exact_y) / k]), color='red',
+              label=exact_title, zorder=3)
+
+    plot.title(title if title else '')
+    plot.legend()
+    plot.savefig('../graphics/' + filename + '.png')
+    plot.clf()
