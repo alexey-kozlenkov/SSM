22c82e4e8766080a3ad3baf49840423abbb35ecb
Alexey Kozlenkov <nidecker95@gmail.com>
2016-03-16 20:18:42 +0300

    Improve histogram plotting

--- util/plotter.py
+++ util/plotter.py
@@ -6,8 +6,8 @@ __author__ = 'Alexey'
 def plot_histogram(sequence, k, filename, title=None):
     plot.xkcd()
     plot.grid(True, linewidth=0.5, zorder=0)
-    plot.ylim(0, len(sequence) * 1.5 / k)
-    plot.hist(sequence, k, zorder=3)
+    n, bins, patches = plot.hist(sequence, k, zorder=3)
+    plot.ylim(0, max(n) * 1.25)
     plot.title(title if title else '')
     plot.savefig('../graphics/' + filename + '.png')
     plot.clf()
