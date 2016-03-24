e5ecfdf2ccb49d60d393f915a2881bd5c00bb7d0
Alexey Kozlenkov <nidecker95@gmail.com>
2016-03-24 21:54:28 +0300

    Plotter improvements

--- lab1/main_lab1.py
+++ lab1/main_lab1.py
@@ -9,7 +9,7 @@ standard_distribution_quantile = 1.959964
 mcg_result = multiplicative_congruential_generator(mcg_data.a0, mcg_data.M, mcg_data.beta, mcg_data.n)
 print 'Multiplicative congruential generator: 1: %f, 100: %f, 900: %f, 1000: %f' % \
       (mcg_result[0], mcg_result[99], mcg_result[899], mcg_result[999])
-plot_histogram(mcg_result, 20, 'mcg', title='Multiplicative congruential generator')
+plot_histogram(mcg_result, 20, 'lab1/mcg', title='Multiplicative congruential generator')
 print 'Method of moments test: ', method_of_moments_test(mcg_result, standard_distribution_quantile)
 print 'Covariation test: ', covariation_test(mcg_result, 30, standard_distribution_quantile)
 print 'Chi-square test: ', chi_square_test(mcg_result, 40)
@@ -22,7 +22,7 @@ sequence_2 = multiplicative_congruential_generator(mmg_data.a, mmg_data.M, max(m
 mmg_result = maclaren_marsaglia_generator(sequence_1, sequence_2, mmg_data.k, 1000)
 print 'MacLaren-Marsaglia generator: 1: %f, 100: %f, 900: %f, 1000: %f' % \
       (mmg_result[0], mmg_result[99], mmg_result[899], mmg_result[999])
-plot_histogram(mmg_result, 20, 'maclaren-marsaglia', title='MacLaren-Marsaglia generator')
+plot_histogram(mmg_result, 20, 'lab1/maclaren-marsaglia', title='MacLaren-Marsaglia generator')
 print 'Method of moments test: ', method_of_moments_test(mmg_result, standard_distribution_quantile)
 print 'Covariation test: ', covariation_test(mmg_result, 30, standard_distribution_quantile)
 print 'Chi-square test: ', chi_square_test(mmg_result, 40)
--- util/plotter.py
+++ util/plotter.py
@@ -13,7 +13,7 @@ def plot_histogram(sequence, k, filename, title=None):
     plot.clf()
 
 
-def plot_histogram_with_exact_function(sequence, k, exact_y, exact_title, filename, title=None):
+def plot_histogram_with_exact_function(sequence, k, exact_y, exact_label, filename, title=None):
     plot.xkcd()
     plot.grid(True, linewidth=0.5, zorder=0)
     n, bins, patches = plot.hist(sequence, k, zorder=3, label='Histogram')
@@ -21,9 +21,23 @@ def plot_histogram_with_exact_function(sequence, k, exact_y, exact_title, filena
 
     bin_centers = [0.5 * (bins[i] + bins[i + 1]) for i in range(k)]
     plot.plot(bin_centers, map(lambda y: y * len(sequence), exact_y[::len(exact_y) / k]), color='red',
-              label=exact_title, zorder=3)
+              label=exact_label, zorder=3)
 
+    plot.xticks()
     plot.title(title if title else '')
     plot.legend()
+    plot.savefig('../graphics/' + filename + '.png')
+    plot.clf()
+
+
+def plot_two_functions(x1, y1, label1, x2, y2, label2, filename, title=None):
+    plot.xkcd()
+    plot.grid(True, linewidth=0.5, zorder=0)
+
+    plot.plot(x1, y1, color='blue', label=label1)
+    plot.plot(x2, y2, color='red', label=label2)
+
+    plot.title(title if title else '')
+    plot.legend(loc=4)
     plot.savefig('../graphics/' + filename + '.png')
     plot.clf()
