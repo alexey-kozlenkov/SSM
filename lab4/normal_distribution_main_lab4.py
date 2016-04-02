00978be036dda689d96c4206d3f78b42642a993b
Alexey Kozlenkov <nidecker95@gmail.com>
2016-04-02 20:36:01 +0300

    lab4: normal distribution main improvements

--- lab4/normal_distribution_main_lab4.py
+++ lab4/normal_distribution_main_lab4.py
@@ -1,3 +1,4 @@
+from math import ceil
 from lab4.function import build_exact_normal_distribution_density
 from util.plotter import plot_histogram, plot_histogram_with_exact_function
 
@@ -9,17 +10,17 @@ s2 = 1
 n = 10000
 N = 42
 
-normal_distribution = normal(m, s2, n, N)
 print 'Normal distribution(%f, %f):' % (m, s2)
-average = sum(normal_distribution) / n
-dispersion = sum((x - average) ** 2 for x in normal_distribution) / (n - 1)
-print '\tActual average: ' + str(average)
-print '\tActual dispersion: ' + str(dispersion)
+normal_distribution_sequence = normal(m, s2, n, N)
+average = sum(normal_distribution_sequence) / n
+dispersion = sum((x - average) ** 2 for x in normal_distribution_sequence) / (n - 1)
+print '\tEmpiric average: ' + str(average)
+print '\tEmpiric dispersion: ' + str(dispersion)
 
-k = 25
-range_bound = max(map(abs, normal_distribution))
-exact_normal_distribution_density = build_exact_normal_distribution_density(m, s2, n)
-plot_histogram_with_exact_function(normal_distribution, k, exact_normal_distribution_density[1],
-                                   'Exact normal distribution', 'lab4/normal_seq', 'Normal distribution(%.3f, %.3f)' % (m, s2),
+k = 40
+range_bound = ceil(max(map(abs, normal_distribution_sequence)))
+exact_normal_distribution_density = build_exact_normal_distribution_density(m, s2, n, -range_bound, range_bound)
+plot_histogram_with_exact_function(normal_distribution_sequence, k, exact_normal_distribution_density[1],
+                                   'Exact normal distribution', 'lab4/normal_seq',
+                                   'Normal distribution (%s=%.3f, %s=%.3f)' % (r'$\mu$', m, r'$\sigma^2$', s2),
                                    (-range_bound, range_bound))
-
