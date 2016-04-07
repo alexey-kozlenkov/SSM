5ddf85aa5108ef2ff350ef2cf4b1f7a2c590258a
Alexey Kozlenkov <nidecker95@gmail.com>
2016-04-08 00:32:34 +0300

    Implement function to plot two histograms

--- util/plotter.py
+++ util/plotter.py
@@ -19,7 +19,7 @@ def plot_histogram_with_exact_function(sequence, k, exact_y, exact_label, filena
     n, bins, patches = plot.hist(sequence, k, range=range_p, normed=True, zorder=3, label='Histogram')
 
     bin_centers = [0.5 * (bins[i] + bins[i + 1]) for i in range(k)]
-    plot.plot(bin_centers, exact_y[::len(exact_y) / k][:k], color='red',  label=exact_label, zorder=3)
+    plot.plot(bin_centers, exact_y[::len(exact_y) / k][:k], color='red', label=exact_label, zorder=3)
 
     plot.xticks()
     plot.title(title if title else '')
@@ -38,4 +38,14 @@ def plot_two_functions(x1, y1, label1, x2, y2, label2, filename, title=None):
     plot.title(title if title else '')
     leg = plot.legend(loc=9, bbox_to_anchor=(0.5, -0.05), ncol=2)
     plot.savefig('../graphics/' + filename + '.png', bbox_inches='tight', additional_artists=(leg,))
+    plot.clf()
+
+
+def plot_two_histograms(sequence1, sequence2, k, label1, label2, filename, title=None, range_p=None):
+    plot.xkcd()
+    plot.hist(sequence1, k, label=label1, range=range_p, normed=True, color='blue', alpha=0.5)
+    plot.hist(sequence2, k, label=label2, range=range_p, normed=True, color='green', alpha=0.5)
+    plot.title(title if title else '')
+    plot.legend(loc='upper right')
+    plot.savefig('../graphics/' + filename + '.png')
     plot.clf()
