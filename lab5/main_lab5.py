8ba4d065d92b2c42a9fc459ed6854c2b0a269000
Alexey Kozlenkov <nidecker95@gmail.com>
2016-05-21 16:59:04 +0300

    lab 5: implemented 3rd task

--- lab5/main_lab5.py
+++ lab5/main_lab5.py
@@ -17,8 +17,12 @@ def f3(x, y):
     return log(1 / sqrt(x ** 2 + y ** 2))
 
 
+def f3_polar(r):
+    return r * log(1. / r)
+
+
 print 'Computing first integral...'
-a, b, N = 0., 1., 1777  # All hail Comte de Buffon!
+a, b, N = 0., 1., 1707  # All hail Comte de Buffon!
 
 base_sequence = builtin_generator(N, a, b)
 f_values = [f1(x) for x in base_sequence]
@@ -28,8 +32,16 @@ print '\tExact (SciPy) value: {:.5f} VS Monte-Carlo value: {:.5f}'.format(exact_
 
 print 'Computing second integral...'
 lmbda = 1.
-p_eta = lambda x: lmbda * exp(- lmbda * x) if x >= 0 else 0
+p_eta = lambda x: lmbda * exp(-lmbda * x) if x >= 0 else 0
 eta_values = exponential(lmbda, N)
 monte_carlo_value = sum(f2(x) / p_eta(x) for x in eta_values) / N
 exact_value = integrate.quad(f2, 0, float('inf'))[0]
+print '\tExact (SciPy) value: {:.5f} VS Monte-Carlo value: {:.5f}'.format(exact_value, monte_carlo_value)
+
+print 'Computing third integral...'
+radius = 2
+base_sequence = builtin_generator(N, 0, radius)
+f_values = [f3_polar(r) for r in base_sequence]
+monte_carlo_value = 2 * pi * float(radius) / N * sum(f_values)
+exact_value = 2 * pi * integrate.quad(lambda r: r * log(1. / r), 0, radius)[0]
 print '\tExact (SciPy) value: {:.5f} VS Monte-Carlo value: {:.5f}'.format(exact_value, monte_carlo_value)
